907f622adab72002bdc0e3508d9e73c649f0590f
Gennadii <rgv451@mail.ru>
2022-03-03 16:40:50 +0700

    Актуалочка

--- src/main/java/com/task1/timesheet/exeptions/OrderNotFoundException.java
+++ src/main/java/com/task1/timesheet/exeptions/OrderNotFoundException.java
@@ -5,5 +5,5 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value= HttpStatus.NOT_FOUND, reason="No such Order")  // 404
 public class OrderNotFoundException extends RuntimeException {
-    // ...
+
 }
