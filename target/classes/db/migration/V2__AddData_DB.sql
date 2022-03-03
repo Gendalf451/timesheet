5b54b3f547483c4d97ae55cd9fc6d09d2751cb31
Gennadii <rgv451@mail.ru>
2022-03-03 16:39:41 +0700

    Актуалочка

---
+++ target/classes/db/migration/V2__AddData_DB.sql
@@ -0,0 +1,34 @@
+insert into tstaff (EMPLOYEE, POSITION, ABSENCE_DATE, ABSENCE_TIME, ABSENCE_REASON)
+values ('Иванов Иван Иванович', 'Слесарь', '12.05.1998', '2', 'Болезнь');
+insert into tstaff (EMPLOYEE, POSITION, ABSENCE_DATE, ABSENCE_TIME, ABSENCE_REASON)
+values ('Иванов Петр Иванович', 'Печник', '12.11.1998', '2', 'Сон');
+insert into tstaff (EMPLOYEE, POSITION, ABSENCE_DATE, ABSENCE_TIME, ABSENCE_REASON)
+values ('Иванов Василий Иванович', 'Водитель', '12.12.1998', '2', 'Усталость');
+insert into tstaff (EMPLOYEE, POSITION, ABSENCE_DATE, ABSENCE_TIME, ABSENCE_REASON)
+values ('Иванов Никита Иванович', 'Учитель', '12.05.2015', '2', 'День рождения');
+
+insert into employee (FULL_NAME)
+values ('Иванов Иван Иванович');
+insert into employee (FULL_NAME)
+values ('Иванов Иван Петрович');
+insert into employee (FULL_NAME)
+values ('Иванов Иван Васильевич');
+insert into employee (FULL_NAME)
+values ('Смирнов Иван Иванович');
+insert into employee (FULL_NAME)
+values ('Чаев Иван Иванович');
+insert into employee (FULL_NAME)
+values ('Кучников Иван Сергеевич');
+
+insert into position (POST)
+values ('Слесарь');
+insert into position (POST)
+values ('Учитель');
+insert into position (POST)
+values ('Дворник');
+insert into position (POST)
+values ('Печник');
+insert into position (POST)
+values ('Менеджер');
+insert into position (POST)
+values ('Сантехник');
